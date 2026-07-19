ac4abaf9ebe8638da5296be9c380934284959b0e
Jaxolotl20112023 <jaxolotl7@gmail.com>
2026-07-19 08:27:45 -1000

    simple pid calibration. need to make it better.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -23,8 +23,7 @@ public class RobotContainer {
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController driver0 = new CommandXboxController(0);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -32,23 +31,9 @@ public class RobotContainer {
     configureBindings();
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    
   }
 
   /**
---
+++ src/main/java/frc/robot/subsystems/Motor.java
@@ -0,0 +1,76 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Motor<T> extends SubsystemBase{
+    
+    private SparkMax m_sparkMax;
+    private SparkFlex m_sparkFlex;  
+    private final CANcoder encoder; 
+
+    private double kp=0,ki=0,kd=0; 
+    private PIDController c_pidController = new PIDController(kp, ki, kd); 
+
+    private double speed; 
+    private double target; 
+    private double positions[] = new double[50];
+    private double tolerance; 
+
+    public Motor(SparkMax m_sparkMax, CANcoder encoder, double target, double tolerance) {
+        this.m_sparkMax = m_sparkMax; 
+        this.encoder = encoder; 
+
+        this.target = target; 
+        this.tolerance = tolerance; 
+
+        c_pidController.setTolerance(this.tolerance);
+    }
+
+    public Motor(SparkFlex m_sparkFlex, CANcoder encoder, double target, double tolerance) {
+        this.m_sparkFlex = m_sparkFlex; 
+        this.encoder = encoder;
+        
+        this.target = target; 
+        this.tolerance = tolerance; 
+
+        c_pidController.setTolerance(this.tolerance);
+    }
+
+    @Override 
+    public void periodic() {
+        speed = c_pidController.calculate(get_position());
+
+        m_sparkMax.set(speed); 
+
+        if (get_position() < target-tolerance) {
+            kp+=0.01; 
+            set_PID(); 
+        } else if (get_position() > target+tolerance) {
+            kp-=0.01; 
+            set_PID();
+        }
+    }
+
+    public void set_setpoint() {
+        c_pidController.setSetpoint(250);
+    }
+
+    public boolean at_setpoint() {
+        return c_pidController.atSetpoint();
+    }
+
+    public double get_position() {
+        return Units.rotationsToDegrees(encoder.getAbsolutePosition().getValueAsDouble());
+    }
+
+    public void set_PID() {
+        c_pidController.setPID(kp, ki, kd);
+    }
+}
